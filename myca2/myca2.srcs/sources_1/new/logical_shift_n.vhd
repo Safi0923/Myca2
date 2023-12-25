07f839f5ef8662d461cfa4e3d002e31c55e4fe15
Safi0923 <128818896+Safi0923@users.noreply.github.com>
2023-12-25 19:42:04 +0100

    minor fix

--- myca2/myca2.srcs/sources_1/new/logical_shift_n.vhd
+++ myca2/myca2.srcs/sources_1/new/logical_shift_n.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
 use work.log_function.all;
 
 -- Uncomment the following library declaration if using
@@ -33,7 +34,7 @@ use work.log_function.all;
 --use UNISIM.VComponents.all;
 
 entity logical_shift_n is
-    generic(WIDTH : positive :=8);
+    generic(WIDTH : positive :=16);
     port (A: in std_logic_vector(WIDTH-1 downto 0);
           S: in std_logic_vector(log2(WIDTH)-1 downto 0);
           D: out std_logic_vector(WIDTH-1 downto 0));
@@ -43,6 +44,7 @@ architecture behavioral of logical_shift_n is
 type two_dim is array (0 to log2(WIDTH)-1) of std_logic_vector(WIDTH-1 downto 0);
 
 component mux is
+generic(WIDTH : positive :=16);
   port(A,B : in std_logic_vector(WIDTH-1 downto 0);
          S   : in std_logic;
          O   : out std_logic_vector(WIDTH-1 downto 0));
@@ -67,9 +69,13 @@ reg: for i in 1 to log2(WIDTH) generate
         );
 end generate;
 
+generate_D1: for i in 1 to log2(WIDTH)-1 generate
+    D1(i) <= D0(((i+1)*WIDTH) -(2**i)-1 downto (i)*WIDTH) & std_logic_vector(to_unsigned(0,2**i));
+end generate;
 
-D1(1) <= D0((1+1)*WIDTH-3 downto (1)*WIDTH) & "00";
-D1(2) <= D0((2+1)*WIDTH-5 downto (2)*WIDTH) & "0000";
+--D1(1) <= D0((1+1)*WIDTH-3 downto (1)*WIDTH) & "00";
+--D1(2) <= D0((2+1)*WIDTH-5 downto (2)*WIDTH) & "0000";
+--D1(3) <= D0((3+1)*WIDTH-9 downto (3)*WIDTH) & "00000000";
 
 D <= D0((log2(WIDTH)+1)*WIDTH-1 downto log2(WIDTH)*WIDTH); 
 
